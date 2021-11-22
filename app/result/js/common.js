fe5582a8b515f88ab502ec6fdeaf7c7d2c78ee65
Igor <membship@ya.ru>
2021-11-22 23:25:55 +0700

    first commit

--- app/result/js/common.js
+++ app/result/js/common.js
@@ -1,216 +1,162 @@
 jQuery(document).ready(function( $ ) {
 
-  $(".toggle-mnu").click(function() {
-    $(this).toggleClass("on");
-    $(".top-mnu").slideToggle();
-    return false;
-  });
-
-  $('body').click(function () {
-    if( $(".toggle-mnu").hasClass("on") ){
-      $(".toggle-mnu").removeClass("on");
-      $(".top-mnu").fadeOut();
-    }
-  });
-
-
-  $(".top-mnu").click(function (e) {
-    e.stopPropagation();
-  });
-
-
-  /************************************/
-
-/*$('.wrapper').prepend('<span class="eye-3"></span>');
-let pg = parseInt(document.location.pathname.match(/\d+/))
-$('body').addClass('active').css('background-image', "url('../img/"+pg+".jpg')");
-$('body:not(.active)').css('background-image', "unset");
-
-$('.eye-3').click(function (e) {
-  e.preventDefault();  
-  $('body').toggleClass('active');
-  let pg = parseInt(document.location.pathname.match(/\d+/));
-  $('body.active').css('background-image', "url('../img/"+pg+".jpg')");
-  $('body:not(.active)').css('background-image', "unset");
-
-});*/
-
-/************************************/
-
-
-  //$("#phone_1").mask("+7 (999) 999-99-99");
-
-//Add Inactive Class To All Accordion Headers
-$('.accordion-header').toggleClass('inactive-header');
-
-  //Set The Accordion Content Width
-  var contentwidth = $('.accordion-header').width();
-  $('.accordion-content').css({'width' : contentwidth });
-  
-  //Open The First Accordion Section When Page Loads
-  $('.accordion-header').first().toggleClass('active-header').toggleClass('inactive-header');
-  $('.accordion-content').first().slideDown().toggleClass('open-content');
-  
-  // The Accordion Effect
-  $('.accordion-header').click(function () {
-    if($(this).is('.inactive-header')) {
-      $('.active-header').toggleClass('active-header').toggleClass('inactive-header').next().slideToggle().toggleClass('open-content');
-      $(this).toggleClass('active-header').toggleClass('inactive-header');
-      $(this).next().slideToggle().toggleClass('open-content');
-    }
-    
-    else {
-      $(this).toggleClass('active-header').toggleClass('inactive-header');
-      $(this).next().slideToggle().toggleClass('open-content');
-    }
-  });
-
-
-  $('.tabs-control-1 .tabs_control_link').click(function (e) {
-    e.preventDefault();
-
-    var item = $(this).closest('.tabs-control-1 .tabs_control_item'),
-    contentItem = $('.tabs_content-1 .tabs_content_item'),
-    itemPosition = item.index();
-
-    contentItem.eq(itemPosition)
-    .add(item)
-    .addClass('active')
-    .siblings()
-    .removeClass('active');
-
-  });
-
-
-  $('.tabs-control-2 .tabs_control_link').click(function (e) {
-    e.preventDefault();
-
-    var item = $(this).closest('.tabs-control-2 .tabs_control_item'),
-    contentItem = $('.tabs_content-2 .tabs_content_item'),
-    itemPosition = item.index();
-
-    contentItem.eq(itemPosition)
-    .add(item)
-    .addClass('active')
-    .siblings()
-    .removeClass('active');
-
-  });
 
 
 
-
-
-  /*$('[data-fancybox="gallery"]').fancybox({
-    arrows: true,
-    infobar: false,
-    smallBtn: false,
-    toolbar: false,
-    iframe : {
-      css : {
-        width : '950px'
-      }
+  var options = {
+    labels: ["43", ""],
+    series: [43, 57],
+    legend: {
+      show: false,
     },    
-    slideClass: "myClass",
-    baseClass: "myclass"
-  });*/
-
-
-  if($('.carous').length) {
-    $('.carous').slick({  
-     dots: false,
-     infinite: true,
-     speed: 300,
-     slidesToShow: 1,   
-   });
-  }
-
-  if($('.pop-cat-w').length) {
-    $('.pop-cat-w').slick({  
-     dots: false,
-     infinite: true,
-     speed: 300,
-     slidesToShow: 4,   
-     responsive: [
-
-     {
-      breakpoint: 1200,
-      settings: {
-        slidesToShow: 3        
+    plotOptions: {
+      pie: {
+        donut: {
+          size: '55%'
+        }
       }
     },
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 2
-      }
+    stroke: {
+      show: true,
+      curve: 'smooth',
+      lineCap: 'butt',
+      colors: undefined,
+      width: 0,
+      dashArray: 0,
     },
-    {
-      breakpoint: 570,
-      settings: {
-        slidesToShow: 1
-      }
+    colors:['#FF4E42', '#FFEEEC'],
+    chart: {
+      type: 'donut',
     },
-    ]
-  });
-  }
+    dataLabels: {
+      enabled: false},
+      responsive: [{
+        breakpoint: 480,
+        options: {
+          chart: {
+            width: 200
+          }
+        }
+      }]
+    };
+
+    var chart = new ApexCharts(document.querySelector(".mobile-before"), options);
+    chart.render();
 
 
-/*
-  infinite: true,
-  speed: 300,
-  slidesToShow: 10,
-  slidesToScroll: 1,
-  centerMode: true,
-  autoplay: false,
-  variableWidth: true,
-  autoplaySpeed: 4000,
-  cssEase: 'ease-out',
-  dots: true
-  */
+    var options = {
+      labels: ["95", ""],
+      series: [95, 5],
+      legend: {
+        show: false,
+      },
+      plotOptions: {
+        pie: {
+          donut: {
+            size: '55%'
+          }
+        }
+      },
+      stroke: {
+        show: true,
+        curve: 'smooth',
+        lineCap: 'butt',
+        colors: undefined,
+        width: 0,
+        dashArray: 0,
+      },
+      colors:['#01CC64', '#E7FAF0'],
+      chart: {
+        type: 'donut',
+      },
+      dataLabels: {
+        enabled: false},
+        responsive: [{
+          breakpoint: 480,
+          options: {
+            chart: {
+              width: 200
+            }
+          }
+        }]
+      };
+
+      var chart = new ApexCharts(document.querySelector(".mobile-after"), options);
+      chart.render();
 
 
 
-  $('.link').click(function(e) {
-    $('.modal-overlay_1').fadeIn();
-    e.preventDefault();
-    $('body').addClass('ohi');
-  });
-
-
-  $('.pop-close, .modal-overlay').click(function(e) {
-    e.preventDefault();
-    $('.modal-overlay').fadeOut();
-    $('body').removeClass('ohi');
-  });
 
 
 
 
-  $('a[href*=\\#]:not([href=\\#])').click(function () {
-    elementClick = $(this).attr("href");
-    destination = $(elementClick).offset().top;
-    $("html:not(:animated),body:not(:animated)").animate({scrollTop: destination - 85}, 1100);
-    return false;
-  });
+      var ctx = document.getElementById("myChart1");
+      var myChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+          labels: ["Переходы из поисковых систем", "Переходы по рекламе", "Прямые заходы"],
+          datasets: [{
+            label: '# of Votes',
+            data: [50,30, 20],
+            backgroundColor: [             
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 159, 64, 0.5)',
+            'rgba(255, 79, 22, 0.5)',
+            ],
+            borderColor: [               
+            'rgba(255, 255, 255, 1)'
+            ],
+            borderWidth: 0
+          }]
+        },
+        options: {
+          rotation: -1.5 * Math.PI,
+          circumference: 1 * Math.PI
+        }
+      });
 
 
-  $(window).scroll(function(){
-    var wt = $(window).scrollTop();  
-    var wh = $(window).height();    
-    if (wt > 600) {
-      $('.serv-arr-up').show(400);
-    }
-    else {
-     $('.serv-arr-up').hide();
-   }
- });
+      var ctx2 = document.getElementById("myChart2");
+      var myChart = new Chart(ctx2, {
+        type: 'doughnut',
+        data: {
+          labels: ["Переходы из поисковых систем", "Переходы по рекламе", "Прямые заходы"],
+          datasets: [{
+            label: '# of Votes',
+            data: [50,32.5, 20],
+            backgroundColor: [             
+            '#01CC64',
+            '#FFA000',
+            '#FF4E42',
+            ],
+            borderColor: [               
+            'rgba(255, 255, 255, 1)'
+            ],
+            borderWidth: 0
+          }]
+        },
+        options: {
+          rotation: 1.5 * Math.PI,
+          circumference: 1 * Math.PI,
+          plugins: {
+            datalabels: {
+              display: true,
+              align: 'bottom',
+              backgroundColor: '#ccc',
+              borderRadius: 3,
+              font: {
+                size: 18,
+              }
+            },
+            
+          }
+        }
+      });
 
-  if($('select').length) {
-    $('select').select2({
-      minimumResultsForSearch: -1
-    });
-  }
+
+
+
+
 
 }); //ready
 
